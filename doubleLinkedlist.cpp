9a3232e0939464508ade2c0981047fdf58aa8a7d
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-13 11:02:07 +0700

    membuat perulangan while

--- doubleLinkedlist.cpp
+++ doubleLinkedlist.cpp
@@ -56,4 +56,10 @@ void addNode()
         // set nilai current = start dan nilai previous = null
         Node *current = START; // step 1.a: start from the first node
         Node *previous = NULL; // step 1.b: previous node is Null initially
-    }
+
+        // Looping selama current != NULL dan noMhs dari current lebih kecil
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                             // step 1.c: traverse the list to find the 
+            previous = current;      // step 1.d: move the previous to the current
+            current = current->next; // step 1.e: move the current to the next node
+        }
