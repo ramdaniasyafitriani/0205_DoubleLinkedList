3de6dd6be38d2aeeab66bd40c7c1c99685d53b7a
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-13 11:10:09 +0700

    kondisi jika previous dan current tidak sama dengan null

--- doubleLinkedlist.cpp
+++ doubleLinkedlist.cpp
@@ -67,3 +67,15 @@ void addNode()
         // set nilai next node baru = current dan prev node baru = previous 
         newNode->next = current; // step 4: make the next field of the new node
         newNode->prev = previous; // step 5: make the previous field of the new node
+
+        // kondisi jika current tidak sama dengan Null
+        if (current != NULL)
+        {
+            current->prev = newNode; // step 6: make the previous field of the current node
+        }
+
+        // kondisi jika previous tidak sama dengan null 
+        if (previous != NULL)
+        {
+            previous->next = newNode; // step 7: make the next field to previous
+        }
