c45a59ef05b2ebe65c618ff208dbf6c6a84d7372
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-13 10:53:36 +0700

    kondisi jika start tidak memiliki nilai

--- doubleLinkedlist.cpp
+++ doubleLinkedlist.cpp
@@ -37,6 +37,12 @@ void addNode()
             cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
             return; 
         }
+        // if the list is empty, make the new node the START
+        // jika list kosong, maka Node nextnya adalah START
+        newNode->next = START; // step 3: make the new node point to the first node
+        // kondisi jika start tidak memiliki nilai atau tidak kosong 
+        if (START != NULL)
+        {
+            START->prev = newNode; // step 4: make the first node point to the node
+        }
  
-    }
-}
