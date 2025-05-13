371c03aa6b8b97c64e8c71547d1d3b11c2677e58
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-13 10:02:46 +0700

    deklarasi noMhs dan name

--- doubleLinkedlist.cpp
+++ doubleLinkedlist.cpp
@@ -4,5 +4,7 @@ using namespace std;
 // membuat struktur untuk object double linked list
 struct Node
 {
-
+    // deklarasi noMhs dan name untuk menampung data
+    int noMhs;
+    string name;
 };
